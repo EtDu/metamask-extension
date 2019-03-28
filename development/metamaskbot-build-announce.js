a765df61bccca896b002706eeb4f1ec0fd125df3
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2019-03-27 22:50:33 -0230

    Remove broken image walkthrough from metamaskbot comment

--- development/metamaskbot-build-announce.js
+++ development/metamaskbot-build-announce.js
@@ -27,19 +27,13 @@ async function start () {
   const FIREFOX = `${BUILD_LINK_BASE}/builds/metamask-firefox-${VERSION}.zip`
   const EDGE = `${BUILD_LINK_BASE}/builds/metamask-edge-${VERSION}.zip`
   const OPERA = `${BUILD_LINK_BASE}/builds/metamask-opera-${VERSION}.zip`
-  const WALKTHROUGH = `${BUILD_LINK_BASE}/test-artifacts/screens/walkthrough%20%28en%29.gif`
 
   const commentBody = `
-  <details>
-    <summary>
-      Builds ready [${SHORT_SHA1}]:
-      <a href="${CHROME}">chrome</a>,
-      <a href="${FIREFOX}">firefox</a>,
-      <a href="${EDGE}">edge</a>,
-      <a href="${OPERA}">opera</a>
-    </summary>
-    <image src="${WALKTHROUGH}">
-  </details>
+    Builds ready [${SHORT_SHA1}]:
+    <a href="${CHROME}">chrome</a>,
+    <a href="${FIREFOX}">firefox</a>,
+    <a href="${EDGE}">edge</a>,
+    <a href="${OPERA}">opera</a>
   `
 
   const JSON_PAYLOAD = JSON.stringify({ body: commentBody })
